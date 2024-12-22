1a8882de34804d404285f6e72b5c1436601a959d
Ofentse Sithole <ST10104487@rcconnect.edu.za>
2024-12-22 14:17:23 +0200

    added the ordinary codes for pages and the css for app.css

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,11 @@
+import '../../App.css'
+
+function About() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/pages/Service.jsx
@@ -0,0 +1,11 @@
+import '../../App.css'
+
+function Service() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default Service
